b6710dbb6fb1f14ab442df3cb4711a7b222f3d01
Gustavo Castro <gustavo-souza96@hotmail.com>
2020-01-27 11:18:09 -0200

    some performance issues

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom'
 
 import Layout from './components/Layout/Layout'
-import Home from './containers/Home/Home';
-import NotFound from './components/NotFound/NotFound'
-import Event from './containers/Event/Event'
-import About from './components/About/About'
+import Spinner from './components/UI/Spinner/Spinner'
+
+const Home = lazy(() => import('./containers/Home/Home'))
+const NotFound = lazy(() => import('./components/NotFound/NotFound'))
+const Event = lazy(() => import('./containers/Event/Event'))
+const About = lazy(() => import('./components/About/About'))
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/event/:id" component={Event} />
-        <Route path="/about" component={About} />
-        <Route path="/not-found" component={NotFound} />
-        <Redirect to="/not-found" />
-      </Switch>
+      <Suspense fallback={<Spinner />}>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/event/:id" component={Event} />
+          <Route path="/about" component={About} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect to="/not-found" />
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
